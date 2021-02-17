b4b989ed64ba96ee73b9580eeb60219693666274
Isaac Allef <isaac_allef@hotmail.com>
2021-02-17 17:40:14 -0300

    first draft of projects layout

--- components/ProjectsLayout.tsx
+++ components/ProjectsLayout.tsx
@@ -1,19 +1,19 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Center, Grid, GridItem, Heading, Icon, Box, Image, Stack, HStack, VStack, SimpleGrid } from "@chakra-ui/react";
 
 export default function ProjectsLayout() {
     return (
-        <Grid
-            h="400px"
-            templateRows="repeat(4, 1fr)"
-            templateColumns="repeat(2, 1fr)"
-            gap={1}
-            >
-                <GridItem colSpan={2} bg="red" />
-                <GridItem colSpan={1} bg="red" />
-                <GridItem colSpan={1} bg="red" />
-                <GridItem colSpan={1} bg="red" />
-                <GridItem colSpan={1} bg="red" />
-                <GridItem colSpan={2} bg="red" />
-        </Grid>
+        <>
+            <Heading variant='title2'>Projects.</Heading>
+            <SimpleGrid columns={2} spacing={10}>
+                <Center height='300px' border='1px solid'></Center>
+                <Center height='300px' border='1px solid'></Center>
+                <Center height='300px' border='1px solid'></Center>
+                <Center height='300px' border='1px solid'></Center>
+            </SimpleGrid>
+            <Center>
+                <Icon as={ChevronDownIcon} w={6} h={6} />
+            </Center>
+        </>
     )
 }
