fe792c04fa5a331cfd814f0996d230ba17f593f4
Isaac Allef <isaac_allef@hotmail.com>
2021-02-19 17:13:19 -0300

    changing grid layout to box layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 // import Head from 'next/head'
 
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import AboutMe from "../components/AboutMe";
 import Foot from "../components/Foot";
 import Header from "../components/Header";
@@ -8,34 +8,17 @@ import ProjectsLayout from "../components/ProjectsLayout";
 import SkillsAndValues from "../components/SkillsAndValues";
 
 export default function Home() {
-  return (
-    <Grid
-    h="1000px"
-    templateRows="repeat(5, 1fr)"
-    templateColumns="repeat(1, 1fr)"
-    gap={0}
-    marginLeft='100px'
-    marginRight='100px'
-    >
-        <GridItem colSpan={1}>
-          <Header />
-        </GridItem>
-        
-        <GridItem colSpan={1}>
-          <AboutMe />
-        </GridItem>
-        
-        <GridItem colSpan={1}>
-          <ProjectsLayout />
-        </GridItem>
-        
-        <GridItem colSpan={1}>
-          <SkillsAndValues />
-        </GridItem>
-        
-        <GridItem colSpan={1}>
-          <Foot />
-        </GridItem>
-    </Grid>
-  )
+    return (
+        <Box
+            as='main'
+            marginLeft='10vw'
+            marginRight='10vw'
+        >
+            <Header />
+            <AboutMe />
+            <ProjectsLayout />
+            <SkillsAndValues />
+            <Foot />
+        </Box>
+    )
 }
