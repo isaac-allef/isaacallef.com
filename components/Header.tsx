467ee162b39143ca528e0964142d0e9f56e4f5dd
Isaac Allef <isaac_allef@hotmail.com>
2021-02-19 17:13:29 -0300

    pinning header to the screen

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,20 +3,23 @@ import { Heading, Flex, HStack, Button } from "@chakra-ui/react";
 export default function Header() {
     return (
         <Flex
-        alignItems='center'
-        justifyContent='space-between'
-        // px='20'
-        py='3'
-        // border= '1px solid'
-        marginBottom='44'
-        marginTop='44'
-      >
-        <Heading variant='title1'>Isaac Allef</Heading>
-        <HStack spacing='6'>
-          <Button variant='link'>Home</Button>
-          <Button variant='link'>About</Button>
-          <Button variant='link'>Contact</Button>
-        </HStack>
-      </Flex>
+            as='header'
+            top={0}
+            position='sticky'
+            justifyContent='space-between'
+            py='1rem'
+            bgColor='white'
+            borderBottom='1px solid'
+            borderColor='gray.100'
+            marginBottom='3rem'
+            marginTop='3rem'
+        >
+            <Heading variant='title1'>Isaac Allef</Heading>
+            <HStack spacing='6'>
+                <Button variant='link'>Home</Button>
+                <Button variant='link'>About</Button>
+                <Button variant='link'>Contact</Button>
+            </HStack>
+        </Flex>
     )
 }
